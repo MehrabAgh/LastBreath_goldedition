e5fdbfc35a4260f4c21cc0b220309da6aae3f3a6
MehrabAgh <aghaeemehrab@gmail.com>
2022-05-02 14:13:37 +0430

    Move Debuging

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -7,35 +7,17 @@ using System;
 public class Movement : MonoBehaviour
 {
     private Rigidbody player;
-    [SerializeField] private float   _speedx , _speedy, SpeedRotate, SpeedJump;
-    private float SpeedX, SpeedY;
+    [SerializeField] private float _speedx , _speedUper, SpeedRotate, SpeedJump;
+    private float SpeedX;
     private bool _jump;
+    private Vector3 MoveDir;
 
     void Start()
     {
         player = GetComponent<Rigidbody>();
+        SpeedX = _speedx;
     }
-
-    private void Forward(float speed, Rigidbody player)
-    {
-        //player.transform.Translate(-Vector3.forward / speed);
-        player.velocity += new Vector3(0,0, Time.deltaTime / -speed);
-    }
-    private void Backward(float speed, Rigidbody player)
-    {
-        // player.transform.Translate(Vector3.forward / speed);
-        player.velocity += Vector3.forward *Time.deltaTime / speed;
-    }
-    private void Left(float speed, Rigidbody player)
-    {
-        //player.transform.Translate(-Vector3.left / speed);
-        player.velocity += new Vector3(Time.deltaTime / -0.5f, 0, 0);
-    }
-    private void Right(float speed, Rigidbody player)
-    {
-        //player.transform.Translate(Vector3.left / speed);
-        player.velocity += new Vector3(Time.deltaTime / speed, 0, 0);
-    }
+ 
     private void Jump(float speed, Rigidbody player)
     {
         if (_jump)
@@ -45,80 +27,31 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void movement()
-    {
-        #region forward_Move 
-        if (Input.GetKey(KeyCode.W))
-        {            
-            SpeedY = _speedy;
-            player.velocity = new Vector3(SpeedX, 0, SpeedY);
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {           
-            SpeedY = 0;
-        }
-        #endregion
-        //
-        #region backward_Move 
-        if (Input.GetKey(KeyCode.S))
-        {           
-            SpeedY = -_speedy;
-            player.velocity += new Vector3(SpeedX, 0, SpeedY);
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {                     
-            SpeedY = 0;
-        }
-        #endregion
-        //
-        #region left_Move 
-        if (Input.GetKey(KeyCode.D))
-        {           
-            SpeedX = _speedx;            
-            player.velocity += new Vector3(SpeedX, 0, SpeedY);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {          
-            SpeedX = 0;
-        }
-        #endregion
-        //        
-        #region right_Move 
-        if (Input.GetKey(KeyCode.A))
-        {         
-            SpeedX = -_speedx;
-            player.velocity += new Vector3(SpeedX, 0, SpeedY);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {           
-            SpeedX = 0;
-        }
-        #endregion
-        //
-        #region Jump
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump(SpeedJump, player);
-        }
-        #endregion
-    }
-    private void Rotation()
-    {       
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.up, SpeedRotate * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.up * SpeedRotate * Time.deltaTime);
-        }
-    }
 
     void Update()
-    {     
-        if (Input.GetKey(KeyCode.LeftShift)) SpeedX = _speedx - 20;
-        movement();       
-        //Rotation();
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) _speedx = _speedUper;
+        if (Input.GetKeyUp(KeyCode.LeftShift)) _speedx = SpeedX;
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_jump)
+            {
+                player.AddForce(transform.up * SpeedJump);
+                _jump = false;
+            }
+        }
+    }
+    void Move(Rigidbody Player)
+    {
+        float hMove = Input.GetAxisRaw("Horizontal");
+        float vMove = Input.GetAxisRaw("Vertical");    
+        Vector3 movement = new Vector3(hMove, 0.0f, vMove);
+        Player.velocity = movement.normalized * _speedx + new Vector3(0.0f, Player.velocity.y, 0.0f);
+    }
+    private void FixedUpdate()
+    {
+        Move(player);
+       
     }
     private void OnCollisionEnter(Collision collision)
     {
